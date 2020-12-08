ab954a4459aeb7c40bcf2e1a4256612e56d3284c
deadletter <ales.akulchyk@gmail.com>
2020-12-08 02:16:27 +0100

    Add stuff

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,34 +1,34 @@
 using UnityEngine;
- using System.Collections;
-  
- public class FollowCamera : MonoBehaviour {
-  
-     public float interpVelocity;
-     public float minDistance;
-     public float followDistance;
-     public GameObject target;
-     public Vector3 offset;
-     Vector3 targetPos;
-     // Use this for initialization
-     void Start () {
-         targetPos = transform.position;
-     }
-     
-     // Update is called once per frame
-     void FixedUpdate () {
-         if (target)
-         {
-             Vector3 posNoZ = transform.position;
-             posNoZ.z = target.transform.position.z;
-  	     float oldY = posNoZ.y;
-             Vector3 targetDirection = (target.transform.position - posNoZ);
-  
-             interpVelocity = targetDirection.magnitude * 5f;
-  
-             targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
-  	     targetPos.y = oldY;
-             transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
-  
-         }
-     }
- }
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+    public float interpVelocity;
+    public float minDistance;
+    public float followDistance;
+    public GameObject target;
+    public Vector3 offset;
+    Vector3 targetPos;
+    // Use this for initialization
+    void Start () {
+        targetPos = transform.position;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+        float oldY = posNoZ.y;
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+
+            interpVelocity = targetDirection.magnitude * 3f;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+        targetPos.y = oldY;
+            transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+
+        }
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,27 +4,35 @@ using UnityEngine.SceneManagement;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D body;
+    private Animator anim;
     private bool faceRight = false;
     private float maxSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
-	body = GetComponent<Rigidbody2D> ();        
+	    body = GetComponent<Rigidbody2D> ();  
+        anim = GetComponent<Animator>();      
     }
 
     // Update is called once per frame
     void Update()
     {
-        float magnitude = body.velocity.y;
-        if (Input.GetKeyDown(KeyCode.Space) && magnitude < 2.5) {
+        //float magnitude = body.velocity.y;
+        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool("IsFlapping") == false) {
         	body.AddForce(new Vector2(0f, 180f));
+            anim.SetTrigger("Flap");
+            anim.SetBool("IsFlapping", true);
         }
         
         float moveX = Input.GetAxis ("Horizontal");
         body.velocity = new Vector2 (moveX * maxSpeed, body.velocity.y);
         
-        if ((moveX > 0 && !faceRight) || (moveX < 0 && faceRight)) {
-            flip();
+        if (moveX > 0 && !faceRight) {
+            anim.SetBool("Turn", true);
+        }
+
+        if ((moveX < 0 && faceRight)) {
+            anim.SetBool("Turn", true);
         }
         
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -34,15 +42,20 @@ public class PlayerControl : MonoBehaviour
 
     }
     
-    void flip() {
+    public void flip() {
+        anim.SetBool("Turn", false);
         faceRight = !faceRight;
         Vector3 scale = transform.localScale;
         transform.localScale = new Vector3(-1*scale.x, scale.y, scale.z);
+         Debug.Log("Flip:  called at: " + Time.time);
+    }
+
+    public void endFlap() {
+        anim.SetBool("IsFlapping", false);
     }
     
     void OnTriggerEnter2D(Collider2D other){
 	if (other.tag == "Spike")
 	    SceneManager.LoadScene("MainScene");
-	    //Application.LoadLevel (Application.loadedLevel);
     }
 }
